e56b312a523f3780ef368248d21e027da7009e56
barutkin <barutkin@gmail.com>
2020-06-07 23:06:16 +0700

    add book and user functions

--- config.php
+++ config.php
@@ -2,9 +2,12 @@
 
 session_start();
 
-defined("DS") ? null : define("DS", DIRECTORY_SEPARATOR);
-defined("DATA_DIR") ? null : define("DATA_DIR", __DIR__ . DS . "data");
-defined("USER_FILENAME") ? null : define("USER_FILENAME", "passwd.csv");
-defined("CATEGORY_FILENAME") ? null : define("CATEGORY_FILENAME", "category");
+defined('DS') ? null : define('DS', DIRECTORY_SEPARATOR);
+defined('DATA_DIR') ? null : define('DATA_DIR', __DIR__ . DS . 'data');
+defined('USER_FILENAME') ? null : define('USER_FILENAME', 'passwd.csv');
+defined('DB_FILENAME') ? null : define('DB_FILENAME', 'db.csv');
+defined('TMP_FILENAME') ? null : define('TMP_FILENAME', 'delme');
+defined('CATEGORY_FILENAME') ? null : define('CATEGORY_FILENAME', 'category');
 
-require_once "functions.php";
+require_once 'userFunctions.php';
+require_once 'bookFunctions.php';
--- install.php
+++ install.php
@@ -2,26 +2,30 @@
 
 session_start();
 
-require_once "config.php";
+require_once 'config.php';
 
-if (!file_exists("DATA_DIR")) {
-    mkdir("DATA_DIR");
+if (!file_exists(DATA_DIR)) {
+    mkdir(DATA_DIR);
 }
 
-if (!file_exists("private/database.csv")) {
-    addBook("Код: тайный язык информатики", "1510", array("Home", "Science", "Best", "New"), "https://www.mann-ivanov-ferber.ru/assets/images/covers/37/20637/1.00x-thumb.png");
-    addBook("Nudge. Архитектура выбора", "1100", array("Home", "Science", "Best"), "https://www.mann-ivanov-ferber.ru/assets/images/covers/81/18481/2.00x-thumb.png");
-    addBook("Удовольствие от x", "1360", array("Home", "Science", "Best"), "https://www.mann-ivanov-ferber.ru/assets/images/covers/92/7392/2.00x-thumb.png");
-    addBook("Притяжение сверхмарафона", "945", array("Home", "Health", "New"), "https://www.mann-ivanov-ferber.ru/assets/images/covers/05/20205/2.00x-thumb.png");
-    addBook("Еда и мозг", "985", array("Home", "Health", "Best"), "https://www.mann-ivanov-ferber.ru/assets/images/covers/93/11693/1.00x-thumb.png");
-    addBook("Китайское исследование", "1045", array("Home", "Health", "Best"), "https://www.mann-ivanov-ferber.ru/assets/images/covers/73/10073/1.00x-thumb.png");
-    addBook("Робким мечтам здесь не место", "1010", array("Home", "Interest", "New"), "https://www.mann-ivanov-ferber.ru/assets/images/covers/33/23933/1.00x-thumb.png");
-    addBook("Гении и аутсайдеры", "1035", array("Home", "Interest", "Best"), "https://www.mann-ivanov-ferber.ru/assets/images/covers/06/3606/1.00x-thumb.png");
-    addBook("Одураченные случайностью", "1035", array("Home", "Interest", "Best"), "https://www.mann-ivanov-ferber.ru/assets/images/covers/17/2917/1.00x-thumb.png");
+if (!userExists('root')) {
+    createUser('root', 'root', 'root');
 }
-if (!userExists("root")) {
-    create_user("root", "root");
+if (!userExists('admin')) {
+    createUser('admin', 'admin', 'admin');
 }
-if (!userExists("guest")) {
-    create_user("guest", "");
+if (!userExists('guest')) {
+    createUser('guest', '', 'guest');
+}
+
+if (!file_exists(DATA_DIR . DS . DB_FILENAME)) {
+    addBook('Код: тайный язык информатики', '1510', array('Home', 'Science', 'Best', 'New'), 'https://www.mann-ivanov-ferber.ru/assets/images/covers/37/20637/1.00x-thumb.png');
+    addBook('Nudge. Архитектура выбора', '1100', array('Home', 'Science', 'Best'), 'https://www.mann-ivanov-ferber.ru/assets/images/covers/81/18481/2.00x-thumb.png');
+    addBook('Удовольствие от x', '1360', array('Home', 'Science', 'Best'), 'https://www.mann-ivanov-ferber.ru/assets/images/covers/92/7392/2.00x-thumb.png');
+    addBook('Притяжение сверхмарафона', '945', array('Home', 'Health', 'New'), 'https://www.mann-ivanov-ferber.ru/assets/images/covers/05/20205/2.00x-thumb.png');
+    addBook('Еда и мозг', '985', array('Home', 'Health', 'Best'), 'https://www.mann-ivanov-ferber.ru/assets/images/covers/93/11693/1.00x-thumb.png');
+    addBook('Китайское исследование', '1045', array('Home', 'Health', 'Best'), 'https://www.mann-ivanov-ferber.ru/assets/images/covers/73/10073/1.00x-thumb.png');
+    addBook('Робким мечтам здесь не место', '1010', array('Home', 'Interest', 'New'), 'https://www.mann-ivanov-ferber.ru/assets/images/covers/33/23933/1.00x-thumb.png');
+    addBook('Гении и аутсайдеры', '1035', array('Home', 'Interest', 'Best'), 'https://www.mann-ivanov-ferber.ru/assets/images/covers/06/3606/1.00x-thumb.png');
+    addBook('Одураченные случайностью', '1035', array('Home', 'Interest', 'Best'), 'https://www.mann-ivanov-ferber.ru/assets/images/covers/17/2917/1.00x-thumb.png');
 }
